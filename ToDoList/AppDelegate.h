14856aec34b104ba02e4969ac57116a623dc86f3
Liu Xiangyu <liuxiangyu@the-new-iMac21-11.local>
2014-12-12 11:55:35 +0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Liu Xiangyu on 12/12/14.
+//  Copyright (c) 2014 Liu Xiangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
